e300dcb9d09fb60d6da81657c9037fcdf5c01603
KIMWOONGJI <KIMWOONGJI@gmail.com>
2022-07-08 00:31:10 +0900

    <TO DO list>
    E - electrical domain
    
    EM - electro-mechanical coupling
    
    M - mechanical domain
    . prepare CAD
    . visualize by 'smimport' to multibody
    
    MA - mechano-acoustical coupling
    . calculate the force factor for diaphragm area
    
    AC - acoustic domain
    . develope the compressor part
    . check impedance over frequency (radiation, horn, parallel section)
    . radiation to SPL 'to multimedia file'
    . SPL meter

---
+++ Geom_Horn.m
@@ -0,0 +1,14 @@
+%% Geometry of horn
+ac.ho.
+
+
+
+%% Geometry of parallel section
+ac.ps.l0    = 0.16826;  % length
+ac.ps.a0    = 
+
+
+
+
+%% Geomtery of compressor
+ac.co.
---
+++ Mat_Prop.m
@@ -0,0 +1,4 @@
+%% Physical properties of air
+air.rho0    = 1.21; % Density,                  [kg/m^3]
+air.c       = 343;  % Speed,                    [m/s]
+air.gamma   = 1.402;% Ratio of specific heat,   [1]
